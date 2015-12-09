fec6861a850cb93cae3ed48b3a3e6bad584952c7
chenxu <zztczcx@163.com>
2015-12-09 15:37:41 +0800

    add toolbar infobox

---
+++ src/components/nav-toolbar-infobox.vue
@@ -0,0 +1,58 @@
+<style lang="less">
+  @import "../less/colors.less";
+
+  .rin-infobox p{
+    white-space: nowrap;
+    color: aliceblue;
+    margin-left: 5px;
+  }
+
+  .rin-logo .rin-infobox {
+    border-radius: 4px;
+    width: 0px;
+    height: 150px;
+    background-color: @color-secondary-1-0;
+    color: blue;
+    position: absolute;
+    top: 20px;
+    left: 0px;
+    -webkit-transition: all 0.2s ease-out;
+  }
+
+  .rin-logo:hover .rin-infobox {
+    left: -210px;
+    width: 200px;
+  }
+
+  .rin-logo .rin-infobox:after {
+    content: "";
+    width: 0; 
+    height: 0; 
+    border-top: 10px solid transparent;
+    border-bottom: 10px solid transparent;
+    border-left: 10px solid @color-secondary-1-0;
+    position: absolute;
+    top: 20px;
+    right: -10px;
+    display: block;
+  }
+
+</style>
+
+<template>
+  <div class="rin-infobox">
+    <p>username: <span>{{user.username}}</span></p>
+
+    <p> 信息 </p>
+    <p> 提示 </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ['user'],
+    data () {
+      return {};
+    },
+  }
+</script>
--- src/components/nav-toolbar.vue
+++ src/components/nav-toolbar.vue
@@ -9,6 +9,18 @@
       cursor: pointer;
       display: inline-block;
       margin: 16px 4px;
+
+      .img-wrap {
+        background-color: @color-secondary-1-4;
+        height: 270px;
+        z-index: 2;
+        position: absolute;
+        margin: 0px;
+        left: 0px;
+        right: 0px;
+        padding-left: 4px;
+      }
+
       img {
         border-radius: 28px;
         border: 4px solid @color-secondary-1-1;
@@ -32,9 +44,12 @@
 <template>
 <div id="rin-toolbar" class="rin-main-bar rin-col">
 
-  <span class="rin-logo">
-    <img src="../assets/user-demo.png" />
-  </span>
+  <div class="rin-logo">
+    <div class="img-wrap">
+      <img src="../assets/user-demo.png" />
+    </div>
+    <info-box v-bind:class="{'show': infobox.visible}" :user="user"></info-box>
+  </div>
 
   <span class="rin-search" v-on:mouseenter="searchBarShow" v-on:mouseleave="searchBarHide" v-on:click="searchBarToggle">
     <i class="material-icons">&#xE8B6;</i>
@@ -56,6 +71,9 @@
         searchBar: {
           visible: false,
           fixed: false
+        },
+        infobox:{
+          visible: false
         }
       };
     },
@@ -68,10 +86,11 @@
       },
       searchBarToggle: function (event) {
         this.searchBar.fixed = !this.searchBar.fixed;
-      },
+      }
     },
     components: {
-      'search-bar': require('./nav-toolbar-search')
+      'search-bar': require('./nav-toolbar-search'),
+      'info-box': require('./nav-toolbar-infobox')
     }
   };
 
