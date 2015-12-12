5cb85d66835210d47e938954aeb9b37e70c0b638
LancerComet <chw644@hotmail.com>
2015-12-13 01:19:39 +0800

    + Search Bar In Process.

--- src/components/nav-toolbar-search.vue
+++ src/components/nav-toolbar-search.vue
@@ -6,6 +6,8 @@
 <!-- SearchBar 样式定义. -->
 <style lang="less">
   @cubicEase: cubic-bezier(.22, .58, .12, .98);
+
+  /* Main Container. | 节点主容器. */
   .rin-search-bar {
     @toolbarWidth : 128px;
     width : calc(~"100% - @{toolbarWidth}");
@@ -19,27 +21,28 @@
     background-color: #fff;
     box-shadow: 0 0 .1em .1em rgba(0,0,0,.3);
     transition: all @cubicEase .5s;
+
     &:after {
-       content: "";
-       display: block;
-       visibility: hidden;
-       clear: both;
-       zoom: 1;
-     }
+      content: "";
+      display: block;
+      visibility: hidden;
+      clear: both;
+      zoom: 1;
+    }
 
     &.show {
-      bottom: -13.8em;
+       bottom: 0;
     }
 
     &.hide {
-      bottom: -100%;
+       bottom: -100%;
     }
 
     &.fixed {
-      bottom: 0
-     }
+       bottom: 0
+    }
 
-     /* Title Part. */
+    /* Title Part. | 标题部分. */
     .material-icons.search {
       vertical-align: top;
     }
@@ -49,40 +52,68 @@
     }
 
 
-    /* Main Container. */
-    /* Left Part. */
-    .left-part {
-      @searchIconWidth : 50px;
-      width : calc(~"100% - @{searchIconWidth}");
+    /* Search List Container. | 搜索结果部分. */
+    .search-list-ctnr {
+      max-height: 25em;
+      padding-bottom: 1em;
+      overflow-x: auto;
+      overflow-y: hidden;
+
+      /* Recent Program List. | 最近番组结果列表. */
+      .recent-program-list {
+        white-space: nowrap;
+      }
+
+      .list-item-title {
+        float: left;
+      }
+
+      .day-item {
+        display: inline-block;
+        height: 25em;
+        vertical-align: top;
+        white-space: normal;
+      }
+
+      .list-item-ctnr {
+        max-width: 30em;
+        width: auto;
+        height: 20em;
+        padding-left: 1em;
+        float: right;
+      }
+
     }
 
-    .team-tag {
-      display: inline-block;
-      background-color: cornflowerblue;
-      padding: .5em;
-      color: #fff;
-      font-size: 12px;
-      border-radius: .5em;
-    }
+    /* Search Bar Container. | 搜索条部分. */
+    .search-bar-ctnr {
+    @searchButtonWidth : 50px;
 
-    .search-input {
-      width: 100%;
-      font-size: 14px;
-      font-family: "Microsoft YaHei";
-    }
+      .search-bar {
+        padding: 1em 0 .5em 0;
+      }
 
-    /* Right Part. */
-    .right-part {
-      width: 50px;
-      text-align: center;
-    }
+      .search-input {
+        width : calc(~"100% - @{searchButtonWidth}");
+        font-size: 14px;
+        font-family: "Microsoft YaHei";
+        float: left;
+      }
 
+      .search-submit-btn {
+        background-color: transparent;
+        width: @searchButtonWidth;
+        border: none;
+        float: right;
+      }
+
+    }
 
     /* Tags Container. */
     .tag-item {
       display: inline-block;
       padding: .5em;
-      margin-right: .5em;
+      margin: .5em .5em .5em 0;
       color: cornflowerblue;
       font-size: 12px;
       border: .1em solid cornflowerblue;
@@ -90,16 +121,30 @@
       cursor: pointer;
       transition: all @cubicEase .4s;
       user-select: none;
+
       &:hover {
-        background-color: cornflowerblue;
-        color: #fff;
-      }
+         background-color: cornflowerblue;
+         color: #fff;
+       }
+
       &:active {
-        background-color: #4fc315;
-        border-color: #4fc315;
-        color: #fff;
-        transition: none !important;
-      }
+         background-color: #4fc315;
+         border-color: #4fc315;
+         color: #fff;
+         transition: none !important;
+       }
+    }
+
+
+
+    .rin-loading-progress {
+      background-color: #fff;
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+      text-align: center;
     }
 
 
@@ -112,6 +157,14 @@
 
     .float-right {
       float: right;
+    }
+
+    .clear-float:after {
+      content: "";
+      display: block;
+      visibility: hidden;
+      clear: both;
+      zoom: 1;
     }
 
     .vertical-middle {
@@ -148,56 +201,62 @@
 
   <div class="rin-search-bar">
 
-      <!-- Preview Title. | 预览标题节点. -->
-      <h4 class="preview-title">
-        <i class="material-icons search">&#xE8B6;</i> <span class="search-bar-title" v-text="searchBarTitle"></span>
-      </h4>
+    <!-- Preview Title. | 预览标题节点. -->
+    <h4 class="preview-title">
+      <i class="material-icons search">&#xE8B6;</i> <span class="search-bar-title">搜索.</span>
+    </h4>
 
-      <!-- Search Result List Container. | 搜索结果容器. -->
-      <div class="search-list-ctnr">
+    <!-- Search Result List Container. | 搜索结果容器. -->
+    <div class="search-list-ctnr clear-float">
 
-        <!-- 最近番剧结果列表. -->
-        <div class="recent-program-list" role="tablist" v-show="nodeControl.recentProgramList.show">
+      <!-- 最近番剧结果列表. -->
+      <div class="recent-program-list clear-float" role="tablist" v-show="nodeControl.recentProgramList.show">
 
-          <!-- 搜索结果节点. -->
-          <div class="list-item" v-for="result in dataObject.recentPrograms" role="listitem">
-            <div class="list-item-title" role="heading" v-text="result.day"></div>
-            <ul class="list-item-ctnr" role="list">
-              <li class="list-item tag-item" v-for="tag in result.tags" role="listitem" v-text="tag"></li>
-            </ul>
-          </div>
+        <!-- 搜索结果节点: 单日内容. -->
+        <div class="day-item" v-for="(day, results) in dataObject.recentPrograms" role="listitem">
 
-        </div>
+          <!-- 当日 Day 标题. -->
+          <div class="list-item-title" role="heading" v-text="day"></div>
 
-        <!-- 建议 Tag 容器. -->
-        <div class="recommend-tags" role="list" v-show="nodeControl.recommendTags.show">
-          <div class="list-item tag-item" role="listitem" v-for="tag in dataObject.recommendTags" v-text="tag"></div>
+          <!-- 当日结果列表. -->
+          <ul class="list-item-ctnr" role="list">
+            <li class="list-item tag-item" v-for="item in results" role="listitem" v-text="item.tag.locale.zh_cn" v-on:click="addUserTag" data-tag="{{item.tag.locale.zh_cn}}"></li>
+          </ul>
+
         </div>
 
       </div>
 
-      <!-- Main Container of SearchBar. | 搜索栏主体容器. -->
-      <div class="main-ctnr clear-float">
-
-          <!-- Left Part. | 左侧节点容器. -->
-          <div class="left-part float-left">
-              <!--<div class="team-tag">团队 Tag</div>-->
-              <input type="text" class="rin-input vertical-middle search-input" v-model="searchBarValue" role="search" aria-label="在这里搜索当前团队内容." placeholder="在这里搜索..." v-on:keyup="getRecommendTags" v-on:keyup.13="searchSubmit">
-          </div>
-
-           <!-- Right Part. | 右侧节点容器. -->
-          <div class="right-part float-right">
-
-              <!-- User-input-tags Container. | 用户已输入标签容器. -->
-              <!--<div class="tags-ctnr">-->
-                  <!--<div class="tag-item" v-for="userTag in userTagList" v-text="userTag" data-value="{{userTag}}" v-on:click="removeTag"></div>-->
-              <!--</div>-->
-
-            <button type="submit" role="button">搜索</button>
-
-          </div>
-
+      <!-- 建议 Tag 容器. -->
+      <div class="recommend-tags" role="list" v-show="nodeControl.recommendTags.show">
+        <div class="list-item tag-item" role="listitem" v-for="tag in dataObject.recommendTags" v-text="tag" data-tag="{{tag}}" v-on:click="addUserTag"></div>
       </div>
+
+      <!-- 搜索进度条 -->
+      <div class="rin-loading-progress" role="progressbar" v-show="nodeControl.loading">
+        <p v-text="loadingText"></p>
+      </div>
+
+    </div>
+
+    <!-- Main Container of SearchBar. | 搜索栏主体容器. -->
+    <div class="search-bar-ctnr clear-float">
+
+      <!-- Search Bar Container. | 搜索栏容器. -->
+      <div class="search-bar clear-float">
+        <!-- 搜索栏本体. -->
+        <input type="text" class="rin-input vertical-middle search-input" v-model="searchBarValue" role="search" aria-label="在这里搜索当前团队内容." placeholder="在这里搜索..." v-on:keyup="getRecommendTags" v-on:keyup.13="searchSubmit">
+
+        <!-- Rock'n Roll! -->
+        <button type="submit" role="button" class="search-submit-btn"><i class="material-icons search">&#xE8B6;</i></button>
+      </div>
+
+      <!-- User-input-tags Container. | 用户已输入标签容器. -->
+      <div class="tags-ctnr">
+        <div class="tag-item" v-for="userTag in userTagList" v-text="userTag" data-value="{{userTag}}" v-on:click="removeUserTag"></div>
+      </div>
+
+    </div>
 
   </div>
 
@@ -210,100 +269,114 @@
 
     // 数据对象定义.
     data () {
-      return {
-        searchBarTitle: "搜索.",  // 搜索节点题文字.
-        searchBarValue: "",  // 搜索框数值.
-        userTagList: ["default1", "default2"],  // 用户输入标签数组.
+    return {
+      searchBarTitle: "搜索.",  // 搜索节点题文字.
+      searchBarValue: "",  // 搜索框数值.
+      loadingText: "正在努力获取最近四天的番组... > <",
+      userTagList: [],  // 用户输入标签数组.
 
-        // 节点控制对象.
-        nodeControl: {
-          recentProgramList: {
-            show: true
-          },
-          recommendTags: {
-            show: false
-          }
+      // 节点控制对象.
+      nodeControl: {
+        recentProgramList: {
+          show: true
+        },
+        recommendTags: {
+          show: false
+        },
+        loading: true
+      },
+
+      // 数据存放对象.
+      dataObject: {
+        // 最近四天番剧数据.
+        recentPrograms: {
+          thursday: [],
+          friday: []
         },
 
-        // 数据存放对象.
-        dataObject: {
-          // 最近四天番剧数据.
-          recentPrograms: [
-            { day: "星期一", tags: ["哲♂学", "新日♂暮里"] },
-            { day: "星期二", tags: ["Deep ♂ Dark ♂ Fantacy", "Boy ♂ Next ♂ Door"]}
-          ],
-
-          // 推荐 Tag 数据:
-          recommendTags: ["啊♂", "屁♂股", "Take ♂ it ♂ Boy"]
-        },
-
-        // 搜索事件相关数据.
-        searchEvent: {
-          recommendTagTimeout: null  // 推荐 Tag 事件延时计时器.
-        }
-      }
-    },
-
-    // 广播事件注册.
-    events: {
-      "recentProgramRequest": function () {
-        this.recentProgramRequest();
-      }
-    },
-
-    // 事件处理函数定义.
-    methods: {
-
-      // 空格增加用户 Tag.
-      addUserTag: function () {
-        if (this.searchBarValue === " ") { this.searchBarValue = null; return; }
-        this.userTagList.push(this.searchBarValue.trim());  // Push Value to [userTagList].
-        this.searchBarValue = null;
+        // 推荐 Tag 数据:
+        recommendTags: ["啊♂", "屁♂股", "Take ♂ it ♂ Boy"]
       },
 
-      // 删除 Tag.
-      removeTag: function (event) {
-        var targetElement = event.srcElement || event.target;
-        var value = targetElement.attributes["data-value"].value;
-        this.userTagList.indexOf(value) > -1 && this.userTagList.splice(this.userTagList.indexOf(value), 1);
-      },
-
-      // Definition: 获取最近四天的番组.
-      recentProgramRequest: function () {
-        // TODO: 发送请求获取最近四天数据.
-        // TODO: this.$http.啪啪啪 ...
-        var self = this;
-        this.searchBarTitle = "正在努力获取最近四天的番组... > <";
-        requestFinished();
-
-        // 请求完成动作.
-        function requestFinished () {
-          // Just testing.
-          setTimeout(function () {
-            self.searchBarTitle = "搜索完成惹！";
-          }, 1000)
-        }
-
-      },
-
-      // 搜索请求事件.
-      searchSubmit: function () {
-        var self = this;
-        self.nodeControl.recentProgramList.show = true;
-        self.nodeControl.recommendTags.show = false;
-      },
-
-      // 获得推荐 Tag 事件.
-      getRecommendTags: function () {
-        var self = this;
-        self.nodeControl.recentProgramList.show = false;
-        self.nodeControl.recommendTags.show = true;
-        clearTimeout(self.searchEvent.recommendTagTimeout);
-        self.searchEvent.recommendTagTimeout = setTimeout(function () {
-          // TODO: 发送请求获取推荐 Tag.
-        });
+      // 搜索事件相关数据.
+      searchEvent: {
+        recommendTagTimeout: null  // 推荐 Tag 事件延时计时器.
       }
     }
+  },
+
+  // 广播事件注册.
+  events: {
+    "recentProgramRequest": function () {
+      this.recentProgramRequest();
+    }
+  },
+
+  // 事件处理函数定义.
+  methods: {
+
+    // Definition: 获取最近四天的番组.
+    recentProgramRequest: function () {
+      // 发送请求获取最近数据.
+      let self = this;
+      self.searchBarTitle = "正在努力获取最近四天的番组... > <";
+      self.$http.get("https://bangumi.moe/api/v2/bangumi/recent", { ts: Date.now() }, requestFinished);
+
+      function requestFinished (result) {
+        // 处理进度条.
+        self.loadingText = "搜索完成惹！";
+        self.nodeControl.loading = false;
+
+        // 数据绑定.
+        self.dataObject.recentPrograms = result;
+      }
+    },
+
+    // Definition: 从最近番组中选择 Tag.
+    addUserTag: function (event) {
+      var tagData = event.target.attributes["data-tag"].value || event.srcElement.attributes["data-tag"].value;
+      if (this.searchBarValue.indexOf(tagData) > -1) { return; }  // 如果已存在该 Tag 则返回.
+      this.searchBarValue += tagData + " ";
+    },
+
+    // Definition: 删除搜索栏中的 Tag.
+    removeUserTag: function (event) {
+      var targetElement = event.srcElement || event.target;
+      var value = targetElement.attributes["data-value"].value;
+      this.userTagList.indexOf(value) > -1 && this.userTagList.splice(this.userTagList.indexOf(value), 1);
+    },
+
+    // Definition: 当提交搜索时生成 Tag.
+    generateTags: function () {
+      this.userTagList = this.searchBarValue.trim().split(" ");
+    },
+
+    // 搜索请求事件.
+    searchSubmit: function () {
+      var self = this;
+      self.userTagList = [];  // 清空之前的 Tag.
+      self.nodeControl.recentProgramList.show = true;
+      self.nodeControl.recommendTags.show = false;
+//        self.generateTags();  // 生成 Tag.
+
+      // TODO: 向服务器发送搜索请求.
+      // ...
+    },
+
+    // 获得推荐 Tag 事件.
+    getRecommendTags: function () {
+      let self = this;
+
+      //
+      self.nodeControl.recentProgramList.show = false;
+      self.nodeControl.recommendTags.show = true;
+      clearTimeout(self.searchEvent.recommendTagTimeout);
+
+      self.searchEvent.recommendTagTimeout = setTimeout(function () {
+        // TODO: 发送请求获取推荐 Tag.
+      });
+    }
+  }
 
   };
 </script>
