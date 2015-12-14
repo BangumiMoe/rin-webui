de37a30be4d2b568cc89b9eb0dadaad88cbf862b
SCys <me@iscys.com>
2015-12-14 14:33:57 +0800

    * rin-user will check is_signined flag

--- src/components/rin-user.vue
+++ src/components/rin-user.vue
@@ -9,17 +9,26 @@
       
       if(!user._id) {
         user = {}
+        is_signined = false;
       } else {
         this.$dispatch('UserSignInOk', user);
+        is_signined = true;
       }
 
       return {
-        user: user
+        user: user,
+        is_signined: is_signined
       }
     },
     methods: {
       'signin' (username, password) {
         let self = this;
+        
+        if(self.is_signined) {
+          self.$dispatch('UserSignInFailed', '本地已经登录，请注销后重试');
+          return;
+        }
+        
         return self.$http.post('/api/user/signin', {username: username, password: SparkMD5.hash(password)}, function(data) {
           if(data.success) {
             self.user = data.user;
@@ -29,10 +38,20 @@
               Cookies.set('rin-user-' + name, value);
             }
             
+            self.is_signined = true;
             self.$dispatch('UserSignInOk', self.user);
           } else {
             self.user = {};
+            self.is_signined = false;
             self.$dispatch('UserSignInFailed', data.message);
+          }
+        }, {
+          xhr: {
+            onerror () {
+              self.user = {};
+              self.is_signined = false;
+              self.$dispatch('UserSignInFailed', 'network or remote server error');
+            }
           }
         })
       },
@@ -49,6 +68,8 @@
         for(var name of ['_id', 'emailHash', 'username']) {
           Cookies.remove('rin-user-' + name);
         }
+        
+        self.is_signined = false;
       }
     },
     events: {
