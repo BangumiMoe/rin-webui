94a9c4bde1c618c7b4d16b505ae66b5147ec6af9
chenxu <zztczcx@163.com>
2015-12-28 02:48:46 +0800

    fix infobox for space of border and container

--- src/components/nav-toolbar-infobox.vue
+++ src/components/nav-toolbar-infobox.vue
@@ -11,41 +11,48 @@
   }
 
   .rin-logo:hover .rin-infobox-arrow-right {
-    left: -235px;
-    width: 235px;
+    left: -237px;
+    width: 237px;
+  }
+
+  .rin-infobox-inner{
+    position: relative;
+    width: 220px;
   }
 
   .rin-infobox {
     border-radius: 10px;
-    border-width: 1px 4px 3px 2px;
-    border-color: @color-primary-2;
-    border-style: solid;
+    /*border-width: 1px 4px 3px 2px;*/
+    /*border-color: @color-primary-2;*/
+    /*border-style: solid;*/
+    margin: 4px;
+	  box-shadow: 0px 0px 0px 2px #fff, 0px 0px 0px 4px #681558;
     padding: 15px 0px;
     width: 220px;
     min-height: 40px;
     max-height: 300px;
     background-color: @color-primary-4;
     color: @color-primary-2;
-    position: relative;
   }
 
-  .rin-infobox:after {
+  .rin-infobox-inner:after {
     content: "";
     width: 0; 
     height: 0; 
     position: absolute;
-    top: 8px;
+    top: 10px;
     display: block;
   }
 
-  .rin-infobox-arrow-right .rin-infobox:after {
-    border-top: 15px solid transparent;
-    border-bottom: 15px solid transparent;
-    border-left: 15px solid @color-primary-4;
-    right: -10px;
+  .rin-infobox-arrow-right .rin-infobox-inner:after {
+    border-top: 9px solid transparent;
+    border-bottom: 9px solid transparent;
+    border-left: 9px solid #681558;
+    right: -15px;
+    top: 10px;
   }
 
-  .rin-infobox-arrow-left .rin-infobox:after {
+  .rin-infobox-arrow-left .rin-infobox-inner:after {
     border-top: 15px solid transparent;
     border-bottom: 15px solid transparent;
     border-right: 15px solid @color-primary-4;
@@ -66,8 +73,10 @@
 
 <template>
   <div class="rin-infobox-wrap rin-infobox-arrow-{{arrow}}">
-    <div class="rin-infobox">
-      <p>Hi, {{user.username || "Anonymous" }}!</p>
+    <div class="rin-infobox-inner">
+      <div class="rin-infobox">
+        <p>Hi, {{user.username || "Anonymous" }}!</p>
+      </div>
     </div>
   </div>
 </template>
